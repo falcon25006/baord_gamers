7f529a463f3f845bf6d25a892f003e5749c4e9ba
YANN-OUEDRAOGO <yannouedraogo04@gmail.com>
2024-10-13 08:58:01 +0200

    first_try_of_navbar_connect_in_all_site

--- accueil.php
+++ accueil.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html lang="fr">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Accueil</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  </head>
-  <body>
-    <div class="container-fluid">
+<div class="container-fluid">
       <nav class="navbar navbar-expand-lg bg-dark navbar-custom navbar-dark">
           <div class="container-fluid ">
             <a class="navbar-brand " href="chess_history.html">CHESS STORY</a>
@@ -33,14 +24,3 @@
           </div>
         </nav>
   </div>
-  <a href="checker.html"></a>
-  <a href="scrabble.html"></a>
-  <a href="chess_history.html"></a>
-
- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
- integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" 
- crossorigin="anonymous">
- </script>
-
-  </body>
-</html>
